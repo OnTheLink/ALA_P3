d67ec80aff59cef27fad4b6c3e46e2aae4539058
Misha Opstal <mishaopstal2004@gmail.com>
2022-04-06 11:14:42 +0200

    Patched <1 & >3999 Romanizer

--- Website/content/internal/js/javascript.js
+++ Website/content/internal/js/javascript.js
@@ -460,21 +460,20 @@ function umRomanizer() {
     document.getElementById('antwoordRoman').innerHTML = "";
     const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
     let a;
-    if(input < 1 || input > 3999)
-    document.getElementById('antwoordRoman').innerHTML = "Voer een getal tussen de 1 en 3999 in...";
-    else{
-      for(let key in romanNumList){
-          a = Math.floor(input / romanNumList[key]);
-          if(a >= 0){
-              for(let i = 0; i < a; i++){
-                roman += key;
-              }
+    if(input < 1 || input > 3999){
+        document.getElementById('antwoordRoman').innerHTML = "Voer een getal tussen de 1 en 3999 in...";
+    }else{
+        for(let key in romanNumList){
+            a = Math.floor(input / romanNumList[key]);
+            if(a >= 0){
+                for(let i = 0; i < a; i++){
+                    roman += key;
+                }
             }
-          input = input % romanNumList[key];
-      }
+            input = input % romanNumList[key];
+        }
+        document.getElementById('antwoordRoman').innerHTML = roman;
     }
-  
-    document.getElementById('antwoordRoman').innerHTML = roman;
-  }
+}
 
 document.getElementById('umRomanBtn').addEventListener("click", umRomanizer);
